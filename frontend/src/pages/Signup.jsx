07d7d375cb59caf1f0e0ab6ef4923aadc2193ffb
saravanakumar <iamsarank@gmail.com>
2024-02-21 12:57:10 +0530

    preview Image

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -3,17 +3,27 @@ import { Link } from "react-router-dom";
 import signupImg from "../assets/images/signup.gif"
 
 const Signup = () => {
-
+  const [selectedFile, setSelectedFile] = useState(null)
+  const [previewURL, setPreviewURL] = useState("")
   const [formData, setFormData] = useState({
     name: "",
     email: "",
     password: "",
-    photo: "",
+    photo: selectedFile,
     gender: "",
   });
 
   const handleInputChange = (e) => {
     setFormData({ ...formData, [e.target.name]: [e.target.value] })
+  }
+
+  const handleFileInputChange = async (event) => {
+    const file = event.target.files[0];
+    console.log(file)
+  }
+
+  const submitHandler = async event => {
+    event.preventDefault()
   }
 
   return (
@@ -30,8 +40,8 @@ const Signup = () => {
             <h3 className="text-headingColor text-[22px] leading-9 font-bold mb-10">
               Create an <span className="text-primaryColor"> Account </span>
             </h3>
-            <form className="py-4 md:py-0">
 
+            <form onSubmit={submitHandler}>
               <div className="mb-5">
                 <input type="text" placeholder="Full Name" name="name" value={formData.name} onChange={handleInputChange}
                   className="w-full px-4 py-3 border-b border-solid border-[#0066ff61] focus:outline-none
@@ -65,19 +75,26 @@ const Signup = () => {
                 </label>
               </div>
 
-              <div className="relative w-[130px] h-[50px]">
-                <input type="file"
-                  name="photo"
-                  id="customFile"
-                  onChange={handleInputChange}
-                  accept=".jpg, .png"
-                  className="absolute top-0 left-0 w-full h-full opacity-0 cursor-pointer" />
+              <div className="mb-5 flex items-center gap-3">
+                <figure className="w-[60px] h-[60px] rounded-full border-2 border-solid border-primaryColor
+                flex items-center justify-center">
+                  <img src="" alt="" className="w-full rounded-full" />
+                </figure>
+                
+                <div className="relative w-[130px] h-[50px]">
+                  <input type="file"
+                    name="photo"
+                    id="customFile"
+                    onChange={handleFileInputChange}
+                    accept=".jpg, .png"
+                    className="absolute top-0 left-0 w-full h-full opacity-0 cursor-pointer" />
 
-                <label
-                  htmlFor="customFile"
-                  className="absolute top-0 left-0 w-full h-full flex items-center px-[0.75rem] py-[0.375rem] 
+                  <label
+                    htmlFor="customFile"
+                    className="absolute top-0 left-0 w-full h-full flex items-center px-[0.75rem] py-[0.375rem] 
             text-[15px] leading-6 overflow-hidden bg-[#0066ff46] text-headingColor font-semibold rounded-lg truncate cursor-pointer">
-                  Upload Photo</label>
+                    Upload Photo</label>
+                </div>
               </div>
               <div className="mt-7">
                 <button type="submit"
