6d45d4d0b062553607ac8ac801313f5b22540440
saravanakumar <iamsarank@gmail.com>
2024-02-22 13:54:55 +0530

    user login complete

--- backend/Controllers/authController.js
+++ backend/Controllers/authController.js
@@ -31,6 +31,7 @@ export const register = async (req, res) => {
         res.status(200).json({ success: true, message: 'User successfully created' })
 
     } catch (err) {
+        
         res.status(500).json({ success: false, message: 'Internal server error, Try again' })
     }
 }
--- backend/Controllers/userController.js
+++ backend/Controllers/userController.js
@@ -2,40 +2,48 @@ import User from '../models/UserSchema.js'
 
 export const updateUser = async (req, res) => {
     const id = req.params.id
+
     try {
         const updatedUser = await User.findByIdAndUpdate(id, { $set: req.body }, { new: true })
-        res.status(200).json({ success: true, message: "Successfully Updated", data: updatedUser })
+
+        res.status(200).json({ success: true, message: 'Successfully updated', data: updatedUser })
     } catch (err) {
-        res.status(500).json({success:false, message:"Failed to Update"})
+        res.status(500).json({ success: false, message: 'Failed to update' })
     }
 }
 
 export const deleteUser = async (req, res) => {
     const id = req.params.id
+
     try {
         await User.findByIdAndDelete(id)
-        res.status(200).json({ success: true, message: "Successfully Deleted"})
+
+        res.status(200).json({ success: true, message: 'Successfully deleted' })
     } catch (err) {
-        res.status(500).json({success:false, message:"Failed to Delete"})
+        res.status(500).json({ success: false, message: 'Failed to delete' })
     }
 }
 
 export const getSingleUser = async (req, res) => {
     const id = req.params.id
+
     try {
         const user = await User.findById(id)
-        res.status(200).json({ success: true, message: "User Found", data:user})
+
+        res.status(200).json({ success: true, message: 'User Found', data: user })
     } catch (err) {
-        res.status(404).json({success:false, message:"User Notfound"})
+        res.status(404).json({ success: false, message: 'User Notfound' })
     }
 }
 
 export const getAllUser = async (req, res) => {
     const id = req.params.id
+
     try {
-        const users = await User.find({})
-        res.status(200).json({ success: true, message: "Users Found", data:users})
+        const users = await User.find({}).select("-password")
+
+        res.status(200).json({ success: true, message: 'User Found', data: users })
     } catch (err) {
-        res.status(404).json({success:false, message:"Users Notfound"})
+        res.status(404).json({ success: false, message: ' Notfound' })
     }
-}
+};
