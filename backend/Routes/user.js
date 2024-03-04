fb60c4a136315c14cb46c3a4a1047351bfadb529
saravanakumar <iamsarank@gmail.com>
2024-03-04 13:42:23 +0530

    profile header

--- backend/Routes/user.js
+++ backend/Routes/user.js
@@ -1,12 +1,13 @@
-import { updateUser, deleteUser, getAllUser, getSingleUser } from '../Controllers/userController.js'
+import { updateUser, deleteUser, getAllUser, getSingleUser, getUserProfile } from '../Controllers/userController.js'
 import express from "express";
 import { authenticate } from '../auth/verifyToken.js';
 
 const router = express.Router()
 
-router.get('/:id',authenticate, getSingleUser);
-router.get('/',authenticate, getAllUser);
-router.put('/:id',authenticate, updateUser);
-router.delete('/:id',authenticate, deleteUser);
+router.get('/:id', authenticate, getSingleUser);
+router.get('/', authenticate, getAllUser);
+router.put('/:id', authenticate, updateUser);
+router.delete('/:id', authenticate, deleteUser);
+router.get('/profile/me', authenticate, getUserProfile);
 
 export default router;
--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -1,9 +1,9 @@
 import { createContext, useContext, useEffect, useReducer } from "react";
 
 const initialState = {
-    user: null,
-    email: null,
-    token: null
+    user: localStorage.getItem('user') !== undefined ? JSON.parse(localStorage.getItem('user')) : null,
+    //email: localStorage.getItem('email') || null,
+    token: localStorage.getItem('token') || null
 }
 
 export const authContext = createContext(initialState);
@@ -13,19 +13,19 @@ const authReducer = (state, action) => {
         case 'LOGIN_START':
             return {
                 user: null,
-                email: null,
+                //email: null,
                 token: null
             };
         case 'LOGIN_SUCCESS':
             return {
                 user: action.payload.user,
-                email: action.payload.email,
+                // email: action.payload.email,
                 token: action.payload.token
             };
         case 'LOGOUT':
             return {
                 user: null,
-                email: null,
+                //email: null,
                 token: null
             };
         default:
@@ -35,8 +35,13 @@ const authReducer = (state, action) => {
 
 export const AuthContextProvider = ({ children }) => {
     const [state, dispatch] = useReducer(authReducer, initialState);
+    useEffect(() => {
+        localStorage.setItem("user", JSON.stringify(state.user));
+        localStorage.setItem("token", state.token);
+        //localStorage.setItem("email",state.user.email);
+    }, [state]);
 
-    return <authContext.Provider value={{ user: state.user, email: state.email, token: state.token, dispatch }}>
+    return <authContext.Provider value={{ user: state.user, token: state.token, dispatch }}>
         {children}
     </authContext.Provider>
 }
